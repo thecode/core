5c451adae812e433701b0b7b9efc56c56ebfb58b
thecode <levyshay1@gmail.com>
2022-04-02 11:04:39 +0000

    New config schema for supporting unique id

--- homeassistant/components/rpi_gpio/__init__.py
+++ homeassistant/components/rpi_gpio/__init__.py
@@ -1,8 +1,6 @@
 """Support for controlling GPIO pins of a Raspberry Pi."""
 import logging
 
-from RPi import GPIO  # pylint: disable=import-error
-
 from homeassistant.const import (
     EVENT_HOMEASSISTANT_START,
     EVENT_HOMEASSISTANT_STOP,
@@ -23,46 +21,44 @@ _LOGGER = logging.getLogger(__name__)
 
 def setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Set up the Raspberry PI GPIO component."""
-    _LOGGER.warning(
-        "The Raspberry Pi GPIO integration is deprecated and will be removed "
-        "in Home Assistant Core 2022.6; this integration is removed under "
-        "Architectural Decision Record 0019, more information can be found here: "
-        "https://github.com/home-assistant/architecture/blob/master/adr/0019-GPIO.md"
-    )
 
     def cleanup_gpio(event):
         """Stuff to do before stopping."""
-        GPIO.cleanup()
+        _LOGGER.info("GPIO.cleanup()")
 
     def prepare_gpio(event):
         """Stuff to do when Home Assistant starts."""
         hass.bus.listen_once(EVENT_HOMEASSISTANT_STOP, cleanup_gpio)
 
     hass.bus.listen_once(EVENT_HOMEASSISTANT_START, prepare_gpio)
-    GPIO.setmode(GPIO.BCM)
+    _LOGGER.info("GPIO.setmode(GPIO.BCM)")
+
     return True
 
 
 def setup_output(port):
     """Set up a GPIO as output."""
-    GPIO.setup(port, GPIO.OUT)
+    _LOGGER.info("GPIO.setup(%s, GPIO.OUT)", port)
 
 
 def setup_input(port, pull_mode):
     """Set up a GPIO as input."""
-    GPIO.setup(port, GPIO.IN, GPIO.PUD_DOWN if pull_mode == "DOWN" else GPIO.PUD_UP)
+    _LOGGER.info("GPIO.setup(%s, GPIO.IN, %s)", port, pull_mode)
 
 
 def write_output(port, value):
     """Write a value to a GPIO."""
-    GPIO.output(port, value)
+    _LOGGER.info("GPIO.output(%s, %s)", port, value)
 
 
 def read_input(port):
     """Read a value from a GPIO."""
-    return GPIO.input(port)
+    _LOGGER.info("GPIO.input(%s)", port)
+    return 1
 
 
 def edge_detect(port, event_callback, bounce):
     """Add detection for RISING and FALLING events."""
-    GPIO.add_event_detect(port, GPIO.BOTH, callback=event_callback, bouncetime=bounce)
+    _LOGGER.info(
+        "GPIO.add_event_detect(%s, GPIO.BOTH, %s, %s)", port, event_callback, bounce
+    )
